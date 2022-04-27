ff800da3cc1fb17bb053297d86c1fc4a8974f4aa
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2022-04-27 12:42:10 +0100

    Update generate.jl

--- generate/generate.jl
+++ generate/generate.jl
@@ -260,7 +260,7 @@ function sidebar_code(book_model)
     <img src="$(SLASH_PREPATH)/assets/julia-logo.svg" style="margin-left:1em; width: 80px; height: auto; vertical-align: middle; display: inline">
     </a>
     <div style="line-height:38px; font-weight: bold; margin-bottom: 0.5em; opacity: 0.6"> $(TERM) | $(INSTITUTION)</div>
-    <h1><a href="https://www.isel.pt/en/mee/electrical-machines-design">$(TITLE)</a></h1>
+    <h1><a href="https://www.isel.pt/en/mee/electrical-machines-design" style="line-height:38px">$(TITLE)</a></h1>
     <h2>$(SUBTITLE)<a href="https://github.com/fonsp/Pluto.jl">
     <img src="$(SLASH_PREPATH)/Pluto_inv.png" style="width: 70px; height: auto; vertical-align: text-bottom"></a></h2>
     <div style="line-height:38px; font-size: 15px; opacity: 0.85"><!--by--> $(INSTRUCTORS)</div>
