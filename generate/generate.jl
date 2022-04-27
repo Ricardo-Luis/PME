f0f432ab312ab94e49f1b6fe780fda3e7dce4aee
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2022-04-27 10:27:31 +0100

    Update generate.jl

--- generate/generate.jl
+++ generate/generate.jl
@@ -79,7 +79,7 @@ available at `https://username.github.io/YourPackage.jl/` then the pre-path shou
 
 # ╔═╡ 88e1e91d-0d48-42e0-b4ab-4866624fd745
 begin
-	PREPATH = "pluto-course-template"
+	PREPATH = "pme"
 	@skip_as_script PREPATH = ""
 end
 
