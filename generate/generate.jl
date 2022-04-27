1dcfc76e7d4d90f478269cbc2b3a45fd88674d1c
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2022-04-27 14:32:51 +0100

    Update generate.jl

--- generate/generate.jl
+++ generate/generate.jl
@@ -58,7 +58,7 @@ md"""
 TITLE = "Electrical Machines Design"
 
 # ╔═╡ a0057e4c-0bcf-4970-8a2b-0412ad5af510
-SUBTITLE = " · reactive notebooks"
+SUBTITLE = "reactive notebooks"
 
 # ╔═╡ 4be56e57-fea0-4fbe-9659-44bed594b1b2
 INSTITUTION = "DEEEA·MEE"
@@ -262,7 +262,7 @@ function sidebar_code(book_model)
     <div style="line-height:38px; font-weight: bold; margin-bottom: 0.5em; opacity: 0.6"> $(TERM) | $(INSTITUTION)</div>
     <h1><a href="https://www.isel.pt/en/mee/electrical-machines-design" style="line-height:38px">$(TITLE)</a></h1>		
     <h2><a href="https://github.com/fonsp/Pluto.jl">
-    <img src="$(SLASH_PREPATH)/Pluto_inv.png" style="width: 70px; height: auto; vertical-align: middle"></a> $(SUBTITLE)</h2>
+    <img src="$(SLASH_PREPATH)/Pluto_inv.png" style="width: 70px; height: auto; vertical-align: middle"></a> · reactive notebooks<!--$(SUBTITLE)--></h2>
     <div style="line-height:38px; font-size: 15px; opacity: 0.85"><!--by--> $(INSTRUCTORS)</div>
     </div>
     <br>
