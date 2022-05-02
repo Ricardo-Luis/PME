896efee4918de2fe3dbf918045260f5b3a4355c7
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2022-05-02 22:53:15 +0100

    Update generate.jl

--- generate/generate.jl
+++ generate/generate.jl
@@ -261,7 +261,7 @@ function sidebar_code(book_model)
     </a>
     <div style="font-weight: bold; margin-bottom: 0.5em; opacity: 0.6"> $(TERM) | $(INSTITUTION)</div>
     <h1><a href="https://www.isel.pt/en/mee/electrical-machines-design">$(TITLE)</a></h1>		
-    <h1> $(SUBTITLE)</h1>
+    <div style="font-size: 13px; font-weight: bold"><em>$(SUBTITLE)</em></div>
     <div style="line-height:28px; font-size: 15px; opacity: 0.85"><!--by--> $(INSTRUCTORS)</div>
     </div>
     <br>
